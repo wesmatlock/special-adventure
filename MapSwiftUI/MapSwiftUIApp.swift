0ade0735d4e8d09dd0b219fe239f0c8ebbbc915a
Wes Matlock <wmatlock@gmail.com>
2024-01-03 15:17:50 -0700

    Initial Commit

---
+++ MapSwiftUI/MapSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapSwiftUIApp.swift
+//  MapSwiftUI
+//
+//  Created by Wesley Matlock on 1/3/24.
+//
+
+import SwiftUI
+
+@main
+struct MapSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
