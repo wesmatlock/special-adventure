82eb437f887ba1af256c80804614c212a1057919
Wes Matlock <wmatlock@gmail.com>
2024-02-28 18:34:34 -0700

    [WIP] Search working with basic routing

--- MapSwiftUI/SearchNearBy.swift
+++ MapSwiftUI/SearchNearBy.swift
@@ -2,19 +2,112 @@ import MapKit
 import SwiftUI
 
 struct SearchNearBy: View {
+
     @State private var locationManager = TPLocationManager.shared
     @State private var position = MapCameraPosition.userLocation(fallback: .automatic)
 
+    // Search
+    @State private var searchText = ""
+    @State private var markers = [MKMapItem]()
+    @State private var visibleRegion: MKCoordinateRegion?
+
+    // selection
+    @State private var selectedMapItem: MKMapItem?
+
+    // directions
+    @State private var route: MKRoute?
+
     var body: some View {
         NavigationStack {
-            Map(position: $position) {
-                UserAnnotation()
+
+            ZStack(alignment: .top) {
+                Map(position: $position, selection: $selectedMapItem) {
+                    ForEach(markers, id: \.self) { marker in
+                        Marker(item: marker)
+                    }
+
+                    UserAnnotation()
+
+                    if let route {
+                        MapPolyline(route)
+                            .stroke(.orange, lineWidth: 6)
+                    }
+                }
+                .onChange(of: locationManager.region) { oldValue, newValue in
+                    withAnimation {
+                        position = .region(locationManager.region)
+                    }
+                }
+                .onMapCameraChange { context in
+                    visibleRegion = context.region
+                }
+
+                VStack {
+                    TextField("Search Map", text: $searchText)
+                        .onSubmit {
+                            fetchSearchResults()
+                        }
+                        .textFieldStyle(.roundedBorder)
+                        .padding(.horizontal)
+                        .padding(.vertical, 5)
+                        .opacity(0.7)
+
+                    if let selectedMapItem {
+                        Button("Get Directions?") {
+                            getDirection(selectedMapItem: selectedMapItem)
+                        }
+                        .buttonStyle(.borderedProminent)
+                    }
+                }
             }
             .navigationTitle("Search Nearby POI")
+
+        }
+    }
+
+    private func fetchSearchResults() {
+        Task {
+            markers = await search(for: searchText, in: visibleRegion ?? locationManager.region)
+        }
+    }
+
+    private func search(for term: String, in region: MKCoordinateRegion?) async -> [MKMapItem] {
+        guard term.count >= 3 else { return [] }
+
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = searchText
+        request.resultTypes = .pointOfInterest
+
+        guard let region = region else { return [] }
+        request.region = region
+
+        let search = MKLocalSearch(request: request)
+        do {
+            let result = try await search.start()
+            return result.mapItems
+        } catch {
+            return []
+        }
+    }
+
+    private func getDirection(selectedMapItem: MKMapItem) {
+        let origin = MKMapItem(placemark: MKPlacemark(coordinate: locationManager.region.center))
+
+        let request = MKDirections.Request()
+        request.source = origin
+        request.destination = selectedMapItem
+        request.requestsAlternateRoutes = false
+
+        Task {
+            let directions = MKDirections(request: request)
+            let results = try await directions.calculate()
+            let routes = results.routes
+            if let firstRoute = routes.first {
+                route = firstRoute
+            }
         }
     }
 }
-
 #Preview {
     SearchNearBy()
 }
--- MapSwiftUI/TPLocationManager.swift
+++ MapSwiftUI/TPLocationManager.swift
@@ -11,7 +11,8 @@ final class TPLocationManager: NSObject, CLLocationManagerDelegate {
 
     var region = MKCoordinateRegion()
 
-    private override init() {
+
+    override init() {
         super.init()
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyThreeKilometers
@@ -23,30 +24,38 @@ final class TPLocationManager: NSObject, CLLocationManagerDelegate {
 
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
         switch manager.authorizationStatus {
-
         case .notDetermined:
             manager.requestWhenInUseAuthorization()
         case .restricted:
             print("Location access is restricted!")
         case .denied:
             print("Location access was denied!")
-        case .authorizedAlways, .authorizedWhenInUse:
+        case .authorizedAlways, .authorizedWhenInUse, .authorized:
             requestLocation()
+
         @unknown default:
             break
         }
     }
 
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        locations.last.map {
-            region = MKCoordinateRegion(
-                center: CLLocationCoordinate2D(latitude: $0.coordinate.latitude, longitude: $0.coordinate.longitude),
-                span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
-
+        locations.last.map { location in
+            region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude), span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
         }
     }
 
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print(error.localizedDescription)
+    }
+}
+
+
+extension MKCoordinateRegion: Equatable {
+    public static func == (lhs: MKCoordinateRegion, rhs: MKCoordinateRegion) -> Bool {
+
+        lhs.center.latitude == rhs .center.latitude &&
+        lhs.center.longitude == rhs.center.longitude &&
+        lhs.span.latitudeDelta == rhs.span.latitudeDelta &&
+        lhs.span.longitudeDelta == rhs.span.longitudeDelta
     }
 }
